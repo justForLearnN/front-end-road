d1b35a34eb6052deaf06a810f3ca3511bbb56158
yangbo <yangbo@tigerbrokers.com>
2017-01-03 14:18:59 +0800

    快速学习前端框架1

--- react/app.js
+++ react/app.js
@@ -1,90 +1,73 @@
-let data = [
-    {
-        id: 1,
-        author: "Pete Hunt",
-        text: "This is one comment"
-    }, {
-        id: 2,
-        author: "Jordan Walke",
-        text: "This is *anothor* comment"
+const Rect = React.createClass({
+  getInitialState: function () {
+    return {
+      isChange: false,
+      width: 100,
+      backgroundColor: 'red'
     }
-];
+  },
+  rectChange: function () {
+    this.state.isChange ? 
+      this.setState({ width: 100, backgroundColor: 'red' }) : 
+      this.setState({ width: this.props.width, backgroundColor: this.props.backgroundColor });
 
-let Comment = React.createClass({
-    rawMarkup: function() {
-        var md =  new Remarkable();
-        var rawMarkup = md.render(this.props.children.toString());
-        return { __html: rawMarkup };
-    },
-    render: function() {
-        return (
-            <div className="comment">
-                <h2 className="commentAuthor">
-                    {this.props.author}
-                </h2>
-                <span dangerouslySetInnerHTML={this.rawMarkup()} />
-            </div>
-        )
+    this.setState({ 
+      isChange: !this.state.isChange 
+    })
+  },
+  render: function () {
+    const boxStyle = {
+      width: `${this.state.width}px`,
+      height: '100px',
+      backgroundColor: this.state.backgroundColor
     }
-});
-
-let CommentList = React.createClass({
-    render: function() {
-        var commentNodes = this.props.data.map(function(comment) {
-            return (
-                <Comment author={comment.author} key={comment.id}>
-                    {comment.text}
-                </Comment>
-            )
-        });
-        return (
-            <div className="commentList">{commentNodes}</div>
-        )
-    }
+    return (
+      <div className="box" style={boxStyle} onClick={this.rectChange}>my first react component!</div>
+    )
+  }
 })
 
-let CommentForm = React.createClass({
-    render: () => {
-        return (
-            <div className="commentForm">Hello, world! I am a CommentForm.</div>
-        )
-    }
-});
+ReactDOM.render(<Rect width="150" backgroundColor="orange"/>, document.querySelector('#content'))
 
-let CommentBox = React.createClass({
-    loadCommentsFromServer: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                console.log(data);
-                this.setState({ data: data })
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, err.toString());
-            }.bind(this)
-        })
-    },
-    getInitialState: function() {
-        return { data: [] }
-    },
-    componentDidMount: function() {
-        this.loadCommentsFromServer();
-        // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-    },
-    render: function() {
-        return (
-            <div className="commentBox">
-                <h1>Coments</h1>
-                <CommentList data={this.state.data} />
-                <CommentForm />
-            </div>
-        )
-    }
-})
+// const CommentBox = React.createClass({
+//     getDefaultProps() {
+//         console.log('getDefaultProps');
+//         return { a: 1 }
+//     },
+//     getInitialState () {
+//         console.log('getInitialState');
+//         setTimeout(() => {
+//             console.log('getInitialState delay');
+//         }, 1000)
+//         return { b: 2 }
+//     },
+//     componentWillMount () {
+//         console.log('componentWillMount');
+//     },
+//     clickFn (event) {
+//         console.log(event.nativeEvent);
+//     },
+//     render () {
+//         console.log('render');
+//         return (
+//             <div className="commentBox" onClick={this.clickFn}>Hello, world! I am a CommentBox {this.props.a}</div>
+//         )
+//     },
 
-ReactDOM.render(
-    <CommentBox url="https://test-frontend-broadcast.laohu8.com/v1/object/1/6017/comments" pollInterval={2000} />,
-    document.querySelector('#content')
-)
+//     componentDidMount () {
+//         console.log('xxxxxxxxx');
+//     }
+// })
+
+// ReactDOM.render(<CommentBox />, document.querySelector('#content'));
+
+
+// 1 基本语法
+// 2 转变思维，从操作DOM到仅仅只操作数据
+// 3 让自己具备模块化思维
+// 4 明白如何创建组件
+// 5 组件之间如何交互，状态，数据如何保存修改传递
+// 6 明白组件生命周期
+// 7 明白最基本的事件交互，如点击，输入信息，并学会查看事件对象中的内容
+// 8 暂时放弃动画交互
+// 9 掌握promise
